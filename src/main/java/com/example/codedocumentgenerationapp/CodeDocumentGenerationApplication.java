0cb11ba3c39d34a3e09cb9b06f7ed41f642739a6
sushant saini <sushantsaini1@gmail.com>
2025-12-12 21:16:57 -0500

    project name change.

---
+++ src/main/java/com/example/codedocumentgenerationapp/CodeDocumentGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.example.codedocumentgenerationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDocumentGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDocumentGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codedocumentgenerationapp/CodeDocumentGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codedocumentgenerationapp
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDocumentGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
